8963d666568cbb2f1800c496104177088a416f4f
Kirill Zhdanov <storenth@yandex.ru>
2022-01-09 17:24:07 +0700

    Add github token regex

--- getsecrets.sh
+++ getsecrets.sh
@@ -21,11 +21,11 @@ BASICAUTH="basic[[:blank:]._-]?auth([\"[:blank:]]+)?([:=])([\"[:blank:]]+)?([[:a
 BEARERTOKEN="bearer[\"[:blank:]._-]?([[:blank:]=]+)?[\"]?([[:alnum:]]){5,}.{0,32}"
 LOCALHOST="(localhost|127\.0\.0\.1|mysql|postgres).{0,32}"
 PRIVATEKEY="(-----BEGIN OPENSSH PRIVATE KEY-----|-----BEGIN RSA PRIVATE KEY-----|-----BEGIN DSA PRIVATE KEY-----|-----BEGIN EC PRIVATE KEY-----|-----BEGIN PGP PRIVATE KEY BLOCK-----).{0,32}"
-
+GITHUBTOKEN="(ghp_|ghu_|ghs_|ghr_|gho_)([[:alnum:]_-]){35,}"
 
 # get js and parse for secrets
 getsecrets(){
-  SECRETS=$($CURLREQUEST ${1} | grep -ioE -e $APITOKEN -e $S3ENDPOINT -e $S3TARGET -e $BASICAUTH -e $BEARERTOKEN -e $LOCALHOST -e $PRIVATEKEY)
+  SECRETS=$($CURLREQUEST ${1} | grep -ioE -e $APITOKEN -e $S3ENDPOINT -e $S3TARGET -e $BASICAUTH -e $BEARERTOKEN -e $LOCALHOST -e $PRIVATEKEY -e $GITHUBTOKEN)
   echo "[+] ${1}"
   echo "${SECRETS}"
   echo
