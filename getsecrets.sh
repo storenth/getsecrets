e33aaef85132adb97b4f940edccb825077577a65
Kirill Zhdanov <storenth@yandex.ru>
2022-01-09 17:57:11 +0700

    Add json token

--- getsecrets.sh
+++ getsecrets.sh
@@ -7,7 +7,7 @@ checkhelp(){
 }
 
 if [ $# -eq 0 ]; then
-  checkhelp "$@" 
+  checkhelp "$@"
 fi
 
 # alias
@@ -20,12 +20,14 @@ S3TARGET="s3://(([[:alnum:].-]+)+)?(([[:alnum:].-]+)+).{0,32}"
 BASICAUTH="basic[[:blank:]._-]?auth([\"[:blank:]]+)?([:=])([\"[:blank:]]+)?([[:alnum:]]){5,}[.]?.{0,32}"
 BEARERTOKEN="bearer[\"[:blank:]._-]?([[:blank:]=]+)?[\"]?([[:alnum:]]){5,}.{0,32}"
 LOCALHOST="(localhost|127\.0\.0\.1|mysql|postgres).{0,32}"
-PRIVATEKEY="(-----BEGIN OPENSSH PRIVATE KEY-----|-----BEGIN RSA PRIVATE KEY-----|-----BEGIN DSA PRIVATE KEY-----|-----BEGIN EC PRIVATE KEY-----|-----BEGIN PGP PRIVATE KEY BLOCK-----).{0,32}"
+# [[:blank:]] need to avoid `grep: parentheses not balanced` issue
+PRIVATEKEY="(-----BEGIN[[:blank:]]OPENSSH[[:blank:]]PRIVATE[[:blank:]]KEY-----|-----BEGIN[[:blank:]]RSA[[:blank:]]PRIVATE[[:blank:]]KEY-----|-----BEGIN[[:blank:]]DSA[[:blank:]]PRIVATE[[:blank:]]KEY-----|-----BEGIN[[:blank:]]EC[[:blank:]]PRIVATE[[:blank:]]KEY-----|-----BEGIN[[:blank:]]PGP[[:blank:]]PRIVATE[[:blank:]]KEY[[:blank:]]BLOCK-----).{0,32}"
 GITHUBTOKEN="(ghp_|ghu_|ghs_|ghr_|gho_)([[:alnum:]_-]){35,}"
+JSONWEBTOKEN="ey([[:alnum:]_=-])+[.]([[:alnum:]_=-])+[.]?([[:alnum:]_=.+/-])+"
 
 # get js and parse for secrets
 getsecrets(){
-  SECRETS=$($CURLREQUEST ${1} | grep -ioE -e $APITOKEN -e $S3ENDPOINT -e $S3TARGET -e $BASICAUTH -e $BEARERTOKEN -e $LOCALHOST -e $PRIVATEKEY -e $GITHUBTOKEN)
+  SECRETS=$($CURLREQUEST ${1} | grep -ioE -e $APITOKEN -e $S3ENDPOINT -e $S3TARGET -e $BASICAUTH -e $BEARERTOKEN -e $LOCALHOST -e $PRIVATEKEY -e $GITHUBTOKEN -e $JSONWEBTOKEN)
   echo "[+] ${1}"
   echo "${SECRETS}"
   echo
